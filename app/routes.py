cbe5ebd3f73c44ccefd63defe969a2da65c622e3
Ilya Shevchenko <ilya.93@hotmail.com>
2020-11-18 20:04:07 -0500

    Added blog pages navigation

--- app/routes.py
+++ app/routes.py
@@ -1,4 +1,4 @@
-from flask import render_template, flash, redirect, url_for, request, jsonify, abort, send_from_directory
+from flask import render_template, flash, redirect, url_for, request, jsonify, abort, send_from_directory, session
 from flask_login import current_user, login_user, logout_user, login_required
 from app import app
 from app import db, POST_DIR
@@ -15,6 +15,7 @@ from pygments.formatters import HtmlFormatter
 from pygments.styles import get_style_by_name
 import frontmatter
 from datetime import datetime
+import math
 
 class Post():
     def __init__(self,name,year):
@@ -26,6 +27,7 @@ def index():
     post_files = [ x[:-3] for x in os.listdir(POST_DIR) ]
     unsorted_files = {}
     post_objects = []
+    post_pages = []
     year_set = set()
     #get the date for the files from the file metadata
     for f in post_files:
@@ -39,11 +41,20 @@ def index():
     #sort the files by date
     sorted_files = sorted(unsorted_files, key=unsorted_files.get)
     sorted_files.reverse()
+    session['posts'] = sorted_files
     #sort the years
     year_list = list(year_set)
     year_list.sort()
     year_list.reverse()
-    return render_template('index.html', posts=sorted_files, year_list=year_list,post_objects=post_objects)
+    #get the total number of post pages
+    post_count = math.ceil(len(sorted_files) / 2)
+    post_pages = [ x for x in range(1, post_count+1) ]
+    return render_template('index.html',
+                            posts=sorted_files,
+                            year_list=year_list,
+                            post_objects=post_objects,
+                            post_pages=post_pages
+                            )
 
 @app.route('/posts/<post>', methods=['GET'])
 def post(post):
@@ -64,3 +75,9 @@ def post(post):
     #combine html and css
     markdown_post = md_css_string + html
     return markdown_post
+
+@app.route('/posts', methods=['GET'])
+def posts():
+    posts = session['posts']
+    print(posts)
+    return jsonify({'posts':posts})
--- app/static/js/base.js
+++ app/static/js/base.js
@@ -1,18 +1,45 @@
 $(document).ready(function(){
-
-    //query post md data
-    $('.post').each(function() {
-        post_name = $(this).attr('id');
-        div = $(this);
-        GetPost(post_name, div)
+    //$('.post').each(function() {
+    //    post_name = $(this).attr('id');
+    //    div = $(this);
+    //    GetPost(post_name, div);
+    //});
+    GetPosts(1);
+    $('.page-num').on("click", function(){
+        GetPosts($(this).text())
     });
-
 })
 
+//query post md data
 function GetPost(post, div){
-    $.get('/posts/'+post+'.md', function(data, data2){
+    $.get('/posts/'+post+'.md', function(data){
+        div.empty()
         div.append(data)
         //add a fluid class to all images. so they scale
         div.find('img').addClass('img-fluid');
+    });
+}
+
+//query posts by page number
+function GetPosts(page_num){
+    $.get('/posts', function(data){
+        posts = data['posts']
+        if (page_num == 1){
+            i=0
+            $('.post').each(function() {
+                post_name = posts[i]
+                div = $(this)
+                GetPost(post_name, div);
+                i++
+            });
+        }else{
+            i=page_num
+            $('.post').each(function() {
+                post_name = posts[i]
+                div = $(this)
+                GetPost(post_name, div);
+                i++
+            });
+        }
     });
 }
