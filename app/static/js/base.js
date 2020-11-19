8e51bc531088ee65411b98647b7cc4ff7baed2b8
Ilya Shevchenko <ilya.93@hotmail.com>
2020-11-19 16:05:57 -0500

    added archive button

--- app/static/js/base.js
+++ app/static/js/base.js
@@ -1,12 +1,34 @@
 $(document).ready(function(){
-    //$('.post').each(function() {
-    //    post_name = $(this).attr('id');
-    //    div = $(this);
-    //    GetPost(post_name, div);
-    //});
+    //select the first posts page on page load
     GetPosts(1);
+
+    //select posts by pages
     $('.page-num').on("click", function(){
         GetPosts($(this).text())
+    });
+
+    //hide archive tab
+    $('.archive').css({'height':'0px','box-shadow':'0px','border':'none','padding':'0px'})
+    $('#archive-button-element').css({'border-radius':'5px', 'border':'1px solid rgb(128, 128, 128)'})
+    $('.archive-year').hide();
+    $('.archive-link').hide();
+
+    //on button archive button click open archive tab
+    $('#archive-button').on("click", function(){
+        if (!$(this).hasClass('archive-open')){
+            $('#archive-button-element').css({'border-radius':'5px 5px 0px 0px', 'border-bottom':'none'})
+            $('.archive').css({'height':'auto','box-shadow':'5px 5px #888888','border':'1px solid rgb(128, 128, 128)','padding':'1em'})
+            $('.archive-year').show();
+            $('.archive-link').show();
+            $(this).addClass('archive-open');
+        }else{
+            $('#archive-button-element').css({'border-radius':'5px', 'border':'1px solid rgb(128, 128, 128)'})
+            $('.archive').css({'height':'0px','box-shadow':'0px','border':'none','padding':'0px'})
+            $('.archive-year').hide();
+            $('.archive-link').hide();
+            $(this).removeClass('archive-open');
+        }
+
     });
 })
 
